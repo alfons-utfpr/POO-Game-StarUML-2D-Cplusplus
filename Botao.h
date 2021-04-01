e854ab8a14e0ec6f1805cc48f467ffaff7bb5965
Anna Caroline <58178338+annaosousa@users.noreply.github.com>
2021-04-01 07:44:07 -0300

    Add files via upload

---
+++ Botao.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Vetor2D.h"
+#include "Cor.h"
+#include "GerenciadorGrafico.h"
+
+#include <string>
+#include <functional>
+
+
+namespace Jogo {
+    class Botao {
+    private:
+        std::function<void(void)> quandoApertado;
+
+    protected:
+        Vetor2F posicao;
+        Vetor2F tamanho;
+        Cor cor;
+        unsigned int tamanhoTexto;
+        std::string texto;
+
+    public:
+        Botao(Vetor2F Posicao = { 0.0f, 0.0f }, Vetor2F Tamanho = { 0.0f, 0.0f }, std::string Texto = "", std::function<void(void)> QuandoApertado = std::function<void(void)>(), unsigned int tamTexto = 15U, Cor c = { 128, 128, 128 });
+        virtual ~Botao();
+        virtual void desenhar(Gerenciador::GerenciadorGrafico& gg) const;
+        Vetor2F getPosicao() const;
+        Vetor2F getTamanho() const;
+        void apertar() const;
+
+    };
+}
