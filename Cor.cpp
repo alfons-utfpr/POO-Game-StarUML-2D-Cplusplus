c7fb89d34ed50be398b2da017c304fff39759d3c
Anna Caroline <58178338+annaosousa@users.noreply.github.com>
2021-04-01 07:45:03 -0300

    Delete Cor.cpp

--- Cor.cpp
+++
@@ -1,5 +0,0 @@
-#include "Cor.h"
-
-namespace exercicio {
-    Cor::Cor(unsigned char R/* = 0*/, unsigned char G/* = 0*/, unsigned char B/* = 0*/, unsigned char A/* = 255*/) : r{ R }, g{ G }, b{ B }, a{ A } {}
-}
